d09cc9fed2535c675acfe8c603ade10dae36d924
warpforge-ci <ci@warpforge.local>
2026-09-16 08:29:57 +0000

    warpforge 0.20.1

--- Casks/warpforge.rb
+++ Casks/warpforge.rb
@@ -1,12 +1,12 @@
 cask "warpforge" do
-  version "0.20.0"
+  version "0.20.1"
 
   on_macos do
-    sha256 "12448aacc320e101c7416fe13087447df77d978fc9c923307d4160fd6bcd3e83"
+    sha256 "59ef93d1d08c4324a4695201b79bf04b1dbea9fd48e74d3cf85398e92bf833e6"
     url "https://github.com/warpforgehq/warpforge/releases/download/v#{version}/Warpforge_#{version}_aarch64.dmg"
   end
   on_linux do
-    sha256 "b63e701f247a98f7a72156f13b9b8e08579fadf98f19dab7e324be16ed7612e9"
+    sha256 "e0a13c0dcf6137f08f0b1d950f2a2c7ac03639f8831189d94c8fb166136e41c5"
     url "https://github.com/warpforgehq/warpforge/releases/download/v#{version}/Warpforge_#{version}_amd64.AppImage"
   end
 
